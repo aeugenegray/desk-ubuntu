08696001228c60b119202a894f3badc9894f1ad6
aeugenegray <34924479+aeugenegray@users.noreply.github.com>
2018-10-10 07:57:27 +0000

    Update one.py

--- one.py
+++ one.py
@@ -2,11 +2,11 @@ import os
 
 
 def install_function():
+    
     os.system('sudo apt-get update')
     os.system('sudo add-apt-repository ppa:mystic-mirage/pycharm')
     os.system('echo deb http://deb.opera.com/opera/ stable non-free | sudo tee /etc/apt/sources.list.d/opera.list')
     os.system('wget -qO - http://deb.opera.com/archive.key | sudo apt-key add -')
-    os.system('sudo apt-key add - ')
     os.system('sudo add-apt-repository ppa:alessandro-strada/ppa -')
     os.system('sudo apt-get update')
     os.system('sudo apt-get install -y sublime-text opera google-drive-ocamlfuse')
