aabf21b0a3b1d973d777ead9a1061c8f6af56ff7
aeugenegray <34924479+aeugenegray@users.noreply.github.com>
2018-10-10 08:09:27 +0000

    Update one.py

--- one.py
+++ one.py
@@ -1,17 +1,15 @@
-import os
+import subprocess
 
+def index():
+    subprocess.call("sudo apt-get update", shell=True)
+    subprocess.call("sudo add-apt-repository ppa:mystic-mirage/pycharm", shell=True)
+    subprocess.call("echo deb http://deb.opera.com/opera/ stable non-free | sudo tee /etc/apt/sources.list.d/opera.list", shell=True)
+    subprocess.call("wget -qO - http://deb.opera.com/archive.key | sudo apt-key add -", shell=True)
+    subprocess.call("sudo add-apt-repository ppa:alessandro-strada/ppa -'", shell=True)
+    subprocess.call("sudo apt-get update", shell=True)
+    subprocess.call("sudo apt-get install -y sublime-text opera google-drive-ocamlfuse", shell=True)
+    subprocess.call("sudo apt-get install -y gimp actionaz terminator gdebi ssvnc chromium-browser", shell=True)
+    subprocess.call("sudo apt-get install -y opera pycharm", shell=True)
 
-def install_function():
-    
-    os.system('sudo apt-get update')
-    os.system('sudo add-apt-repository ppa:mystic-mirage/pycharm')
-    os.system('echo deb http://deb.opera.com/opera/ stable non-free | sudo tee /etc/apt/sources.list.d/opera.list')
-    os.system('wget -qO - http://deb.opera.com/archive.key | sudo apt-key add -')
-    os.system('sudo add-apt-repository ppa:alessandro-strada/ppa -')
-    os.system('sudo apt-get update')
-    os.system('sudo apt-get install -y sublime-text opera google-drive-ocamlfuse')
-    os.system('sudo apt-get install -y gimp actionaz terminator gdebi ssvnc chromium-browser')
-    os.system('sudo apt-get install -y opera pycharm')
-    
-    install_function()
+index()
     
